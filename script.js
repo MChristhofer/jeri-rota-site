53fe387cee99145b6b42d9c1c4bccdb16076fd2e
Markus Catunda <MARKUSCATUNDA6@GMAIL.COM>
2025-05-20 00:29:09 -0300

    atualiazado

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const toggle = document.getElementById('mobile-menu-toggle');
-  const mobileNav = document.getElementById('mobile-nav');
-
-  toggle.addEventListener('click', () => {
-    mobileNav.classList.toggle('show');
-  });
-});
